858cf138926aee36d67da932c464c78fdf9ec7f9
yuzhe-428 <2775769682@qq.com>
2026-04-17 00:05:39 +0800

    第二次上传pvz相关代码

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,136 +1,161 @@
 #include "mainwindow.h"
 #include <QFont>
+#include <QPainter>
+#include <QPixmap>
 
-MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-{
-    setFixedSize(900, 600);
-    gameTimer = new QTimer(this);
-    gameTimer->start(30);
-    connect(gameTimer, &QTimer::timeout, this, &MainWindow::updateGame);
-
-    zombies.push_back({{800, 200}, 3, false});
+MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent) {
+    setFixedSize(800, 600);
+    setWindowTitle("植物大战僵尸：肉鸽幸存者");
+    playerPos = QPointF(50, 300);
     lastShootTime = QTime::currentTime();
-    lastSunTime = QTime::currentTime();
-    sunCount = 50;
+
+    timer = new QTimer(this);
+    connect(timer, &QTimer::timeout, this, &MainWindow::gameLoop);
+    timer->start(16);
 }
 
-MainWindow::~MainWindow()
-{
+MainWindow::~MainWindow() {
+    // 防止内存泄漏
+    qDeleteAll(zombies);
+    qDeleteAll(bullets);
 }
 
-void MainWindow::paintEvent(QPaintEvent *)
-{
-    QPainter p(this);
-    p.fillRect(rect(), QColor(34, 139, 34));
+void MainWindow::keyPressEvent(QKeyEvent *event) {
+    if(isPaused) {
+        int choice = -1;
+        if(event->key() == Qt::Key_1) choice = 0;
+        else if(event->key() == Qt::Key_2) choice = 1;
+        else if(event->key() == Qt::Key_3) choice = 2;
 
-    for(int i=0; i<9; i++){
-        for(int j=0; j<5; j++){
-            p.drawRect(i*100, j*120, 100, 120);
+        if(choice >= 0 && choice < currentOptions.size()) {
+            Skill s = currentOptions[choice];
+            if(s.type == 0) fireRate = qMax(150, fireRate - 150);
+            else if(s.type == 1) bulletCount++;
+            else if(s.type == 2) bulletDamage += 2;
+            isPaused = false;
+            timer->start();
         }
+        return;
     }
 
-    p.setBrush(Qt::green);
-    for(auto& plant : plants)
-        p.drawEllipse(plant, 30, 30);
-
-    for(auto& z : zombies) {
-        if(z.isRoadBlock) {
-            p.setBrush(Qt::darkGray);
-        } else {
-            if(z.hp >= 5) p.setBrush(Qt::red);
-            else if(z.hp >= 3) p.setBrush(QColor(255,150,0));
-            else p.setBrush(QColor(139,69,19));
-        }
-        p.drawEllipse(z.pos, 35, 35);
-    }
-
-    p.setBrush(Qt::white);
-    for(auto& b : bullets)
-        p.drawEllipse(b, 8, 8);
-
-    p.setBrush(Qt::yellow);
-    for(auto& sun : suns)
-        p.drawEllipse(sun, 20, 20);
-
-    p.setPen(Qt::white);
-    p.setFont(QFont("Arial", 20));
-    p.drawText(20, 40, "阳光: " + QString::number(sunCount));
+    if(event->key() == Qt::Key_W && playerPos.y() > 50) playerPos.ry() -= 30;
+    if(event->key() == Qt::Key_S && playerPos.y() < 550) playerPos.ry() += 30;
 }
 
-void MainWindow::mousePressEvent(QMouseEvent *e)
-{
-    QPointF pos = e->position();
-    int x = pos.x();
-    int y = pos.y();
-
-    for(int i=0; i<suns.size(); i++){
-        if(qAbs(x - suns[i].x())<25 && qAbs(y - suns[i].y())<25){
-            suns.removeAt(i);
-            sunCount +=25;
-            return;
-        }
-    }
-
-    if(sunCount >=50){
-        plants.append(pos.toPoint());
-        sunCount -=50;
-    }
+void MainWindow::spawnZombie() {
+    Zombie* z = new Zombie();
+    z->pos = QPointF(850, QRandomGenerator::global()->bounded(50, 550));
+    z->isFast = QRandomGenerator::global()->bounded(10) > 8;
+    z->hp = 2 + (level / 2);
+    zombies.append(z);
 }
 
-void MainWindow::updateGame()
-{
-    for(int i=0; i<zombies.size(); i++)
-        zombies[i].pos.setX(zombies[i].pos.x()-1);
+void MainWindow::showLevelUpMenu() {
+    isPaused = true;
+    timer->stop();
+    currentOptions.clear();
+    currentOptions.append({"超级射速", "发射频率大幅提升", 0});
+    currentOptions.append({"多重射击", "一次发射更多子弹", 1});
+    currentOptions.append({"重型弹药", "子弹伤害翻倍", 2});
+    update();
+}
 
-    for(int i=0; i<zombies.size(); i++){
-        if(zombies[i].pos.x() <0){
-            zombies.removeAt(i);
-            break;
+void MainWindow::gameLoop() {
+    if(lastShootTime.msecsTo(QTime::currentTime()) > fireRate) {
+        for(int i=0; i<bulletCount; ++i) {
+            Bullet* b = new Bullet();
+            b->pos = playerPos + QPointF(30, 0);
+            b->damage = bulletDamage;
+            float spread = (i - (bulletCount-1)/2.0f) * 1.0f;
+            b->velocity = QVector2D(6.0f, spread);
+            bullets.append(b);
         }
-    }
-
-    if(zombies.isEmpty()){
-        int row = QRandomGenerator::global()->bounded(0,5);
-        bool road = QRandomGenerator::global()->bounded(2) == 0;
-        zombies.push_back({{800, row*120+60}, road ? 6 : 3, road});
-    }
-
-    if(QTime::currentTime().msecsTo(lastSunTime) <=-3000){
-        int x = QRandomGenerator::global()->bounded(100,800);
-        int y = QRandomGenerator::global()->bounded(50,500);
-        suns.append({x,y});
-        lastSunTime = QTime::currentTime();
-    }
-
-    if(QTime::currentTime().msecsTo(lastShootTime) <=-2000){
-        for(auto& plant : plants)
-            bullets.append({plant.x()+30, plant.y()});
         lastShootTime = QTime::currentTime();
     }
 
-    for(int i=0; i<bullets.size(); i++)
-        bullets[i].setX(bullets[i].x()+3);
+    // 使用 std::as_const 或 const 引用遍历指针列表，解决 detach 警告
+    for(auto z : std::as_const(zombies)) z->move();
+    for(auto b : std::as_const(bullets)) b->move();
 
-    for(int i=0; i<bullets.size(); i++){
-        if(bullets[i].x()>width()){
-            bullets.removeAt(i);
-            break;
+    for(int i = bullets.size()-1; i>=0; --i) {
+        if(bullets[i]->pos.x() > 800) {
+            delete bullets.takeAt(i);
+            continue;
         }
-    }
-
-    for(int i=0; i<bullets.size(); i++){
-        for(int j=0; j<zombies.size(); j++){
-            auto& b = bullets[i];
-            auto& z = zombies[j];
-            if(qAbs(b.x()-z.pos.x())<40 && qAbs(b.y()-z.pos.y())<40){
-                bullets.removeAt(i);
-                z.hp -=1;
-                if(z.hp <=0) zombies.removeAt(j);
-                return;
+        for(int j = zombies.size()-1; j>=0; --j) {
+            if(QLineF(bullets[i]->pos, zombies[j]->pos).length() < 30) {
+                zombies[j]->hp -= bullets[i]->damage;
+                delete bullets.takeAt(i);
+                if(zombies[j]->hp <= 0) {
+                    delete zombies.takeAt(j);
+                    score += 10;
+                    exp++;
+                    if(exp >= expToNextLevel) {
+                        level++;
+                        exp = 0;
+                        expToNextLevel += 3;
+                        showLevelUpMenu();
+                    }
+                }
+                break;
             }
         }
     }
 
+    for(int i = zombies.size()-1; i>=0; --i) {
+        if(zombies[i]->pos.x() < -50) {
+            delete zombies.takeAt(i);
+            score = qMax(0, score - 20);
+        }
+    }
+
+    if(QRandomGenerator::global()->bounded(100) < 5) spawnZombie();
     update();
+}
+
+void MainWindow::paintEvent(QPaintEvent *) {
+    QPainter p(this);
+    p.setRenderHint(QPainter::Antialiasing);
+
+    // 1. 绘制背景图 (填满整个窗口)
+    QPixmap bg(":/images/background.png");
+    if(!bg.isNull()) {
+        p.drawPixmap(rect(), bg);
+    } else {
+        // 如果图片加载失败，备用深绿色背景
+        p.fillRect(rect(), QColor(34, 139, 34));
+    }
+
+    // 2. 画玩家 (植物)
+    // 根据你的 mainwindow.h，变量名是 playerPos
+    p.drawPixmap(playerPos.x() - 30, playerPos.y() - 30, 60, 60, QPixmap(":/images/plant.png"));
+
+    // 3. 画僵尸
+    for(auto z : std::as_const(zombies)) {
+        // 使用 -> 访问指针，路径对应 qrc
+        p.drawPixmap(z->pos.x() - 35, z->pos.y() - 35, 70, 70, QPixmap(":/images/zombie.png"));
+    }
+
+    // 4. 画子弹
+    for(auto b : std::as_const(bullets)) {
+        p.drawPixmap(b->pos.x() - 10, b->pos.y() - 10, 20, 20, QPixmap(":/images/bullet.png"));
+    }
+
+    // 5. 绘制 UI 文字
+    p.setPen(Qt::white);
+    p.setFont(QFont("Microsoft YaHei", 12, QFont::Bold));
+    p.drawText(20, 30, QString("等级: %1  经验: %2/%3  分数: %4")
+                           .arg(level).arg(exp).arg(expToNextLevel).arg(score));
+
+    // 6. 升级菜单逻辑
+    if(isPaused) {
+        p.fillRect(rect(), QColor(0, 0, 0, 180)); // 半透明遮罩
+        p.setFont(QFont("Arial", 20, QFont::Bold));
+        p.drawText(rect(), Qt::AlignTop | Qt::AlignHCenter, "\n\nLEVEL UP! CHOOSE A BUFF");
+        p.setFont(QFont("Arial", 14));
+        for(int i=0; i<currentOptions.size(); i++) {
+            p.drawText(250, 250 + i*60, QString("按 [%1]: %2 (%3)")
+                                              .arg(i+1).arg(currentOptions[i].name).arg(currentOptions[i].desc));
+        }
+    }
 }
--- mainwindow.h
+++ mainwindow.h
@@ -4,41 +4,78 @@
 #include <QMainWindow>
 #include <QTimer>
 #include <QPainter>
-#include <QMouseEvent>
+#include <QKeyEvent>
 #include <QList>
 #include <QTime>
 #include <QRandomGenerator>
+#include <QVector2D>
 
-struct Zombie {
-    QPoint pos;
+// 1. 基础类：体现 OOP 思想
+class GameObject {
+public:
+    QPointF pos;
     int hp;
-    bool isRoadBlock;
+    virtual void move() = 0;
+    virtual ~GameObject() {}
 };
 
-class MainWindow : public QMainWindow
-{
+// 2. 僵尸子类
+class Zombie : public GameObject {
+public:
+    bool isFast;
+    void move() override { pos.rx() -= (isFast ? 3.0 : 1.5); }
+};
+
+// 3. 子弹子类
+class Bullet : public GameObject {
+public:
+    int damage;
+    QVector2D velocity;
+    void move() override { pos += velocity.toPointF(); }
+};
+
+// 4. 技能结构
+struct Skill {
+    QString name;
+    QString desc;
+    int type;
+};
+
+class MainWindow : public QMainWindow {
     Q_OBJECT
 
 public:
     MainWindow(QWidget *parent = nullptr);
-    ~MainWindow();
+    ~MainWindow(); // 必须析构释放内存
 
 protected:
     void paintEvent(QPaintEvent *event) override;
-    void mousePressEvent(QMouseEvent *event) override;
+    void keyPressEvent(QKeyEvent *event) override;
 
 private slots:
-    void updateGame();
+    void gameLoop();
 
 private:
-    QTimer* gameTimer;
-    QList<QPoint> plants;
-    QList<Zombie> zombies;
-    QList<QPoint> bullets;
+    void spawnZombie();
+    void showLevelUpMenu();
 
-    QList<QPoint> suns;
-    int sunCount;
-    QTime lastSunTime;
+    QTimer *timer;
+    QPointF playerPos;
+    QList<Zombie*> zombies;
+    QList<Bullet*> bullets;
+
+    // 游戏数值
+    int score = 0;
+    int level = 1;
+    int exp = 0;
+    int expToNextLevel = 5;
+    bool isPaused = false;
+    QList<Skill> currentOptions;
+
+    // 强化属性
+    int fireRate = 800;
+    int bulletCount = 1;
+    int bulletDamage = 1;
     QTime lastShootTime;
 };
 
